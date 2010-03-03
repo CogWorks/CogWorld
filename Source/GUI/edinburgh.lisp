02a342f6c1f7f4cee7d699cb54202dfecc3b4432
Ryan Hope <hoper2@rpi.edu>
2010-03-03 01:59:40 -0500

    got basic likert working

--- Source/GUI/edinburgh.lisp
+++ Source/GUI/edinburgh.lisp
@@ -1,3 +1,37 @@
+(defmacro likert-button-left (label)
+  `(make-instance 'capi:radio-button
+                  :text ""
+                  :title ,label
+                  :title-position :left))
+
+(defmacro likert-button-right (label)
+  `(make-instance 'capi:radio-button
+                  :text ""
+                  :title ,label
+                  :title-position :right
+                  :title-gap -10))
+
+(defmacro likert-button-center ()
+  `(make-instance 'capi:radio-button
+                  :text ""))
+
+(defmacro likert-button-list (&rest labels)
+  `(let ((len (list-length ',labels))
+         (likert-buttons '()))
+     (let ((mid (/ len 2.0)))
+       (let ((sidelen (floor mid)))
+         (loop for i from 0 to (1- sidelen) do
+               (push (likert-button-right (nth i (reverse ',labels))) likert-buttons))
+         (cond
+          ((/= mid sidelen)
+           (push (likert-button-center) likert-buttons)
+           (loop for i from (1+ sidelen) to (1- len) do
+                 (push (likert-button-left (nth i (reverse ',labels))) likert-buttons)))
+          (t (loop for i from sidelen to (1- len) do
+                 (push (likert-button-left (nth i (reverse ',labels))) likert-buttons)))
+          )))
+     likert-buttons))
+          
 (capi:define-interface edinburgh ()
   ()
   (:panes
@@ -8,45 +42,45 @@
                        :family :stock
                        :weight :bold))
    (row1 capi:radio-button-panel
-          :title "Writing:"
-          :title-position :left
-          :items '("Left" "No Pref" "Right"))
+         :title "Writing:"
+         :title-position :left
+         :items (likert-button-list "Left" "" "Right"))
    (row2 capi:radio-button-panel
           :title "Drawing:"
           :title-position :left
-          :items '("Left" "No Pref" "Right"))
+          :items (likert-button-list "Left" "" "Right"))
    (row3 capi:radio-button-panel
           :title "Throwing:"
           :title-position :left
-          :items '("Left" "No Pref" "Right"))
+          :items (likert-button-list "Left" "" "Right"))
    (row4 capi:radio-button-panel
           :title "Using Scissors:"
           :title-position :left
-          :items '("Left" "No Pref" "Right"))
+          :items (likert-button-list "Left" "" "Right"))
    (row5 capi:radio-button-panel
           :title "Using a Toothbrush:"
           :title-position :left
-          :items '("Left" "No Pref" "Right"))
+          :items (likert-button-list "Left" "" "Right"))
    (row6 capi:radio-button-panel
           :title "Using a Knife (without a fork):"
           :title-position :left
-          :items '("Left" "No Pref" "Right"))
+          :items (likert-button-list "Left" "" "Right"))
    (row7 capi:radio-button-panel
           :title "Using a Spoon:"
           :title-position :left
-          :items '("Left" "No Pref" "Right"))
+          :items (likert-button-list "Left" "" "Right"))
    (row8 capi:radio-button-panel
           :title "Using a broom (upper hand):"
           :title-position :left
-          :items '("Left" "No Pref" "Right"))
+          :items (likert-button-list "Left" "" "Right"))
    (row9 capi:radio-button-panel
           :title "Striking a Match:"
           :title-position :left
-          :items '("Left" "No Pref" "Right"))
+          :items (likert-button-list "Left" "" "Right"))
    (row10 capi:radio-button-panel
           :title "Opening a Box (holding the lid):"
           :title-position :left
-          :items '("Left" "No Pref" "Right"))
+          :items (likert-button-list "Left" "" "Right"))
    )
   (:layouts
    (main capi:column-layout '(description inventory) :adjust :left)
