f29ba0c5aa860d4758e1a1a3cd366f14f0b95182
Ryan Hope <hoper2@rpi.edu>
2010-03-03 17:31:07 -0500

    layout is probably as good as I can get it for now

--- Source/GUI/edinburgh.lisp
+++ Source/GUI/edinburgh.lisp
@@ -5,17 +5,48 @@
 (capi:define-interface edinburgh ()
   ()
   (:panes
-   (description capi:display-pane
-                :text "Which hand do you prefer to use when:"
+   (title capi:display-pane
+                :text "Handedness Questionnaire"
                 :background :transparent
                 :font (gp:make-font-description
                        :family :stock
+                       :size 20
                        :weight :bold))
+   (instructions capi:display-pane
+                :text "Instructions"
+                :background :transparent
+                :font (gp:make-font-description
+                       :family :stock
+                       :size 16
+                       :weight :bold))
+   (inst capi:display-pane
+                :text "For each of the activities below, please indicate:"
+                :background :transparent
+                :font (gp:make-font-description
+                       :family :stock
+                       :size 15
+                       :weight :medium))
+   (inst1 capi:display-pane
+                :text "Which hand you prefer for that activity?"
+                :background :transparent
+                :font (gp:make-font-description
+                       :family :stock
+                       :size 12
+                       :weight :light
+                       :slant :italic))
+   (inst2 capi:display-pane
+                :text "Do you ever use the other hand for the activity?"
+                :background :transparent
+                :font (gp:make-font-description
+                       :family :stock
+                       :size 12
+                       :weight :light
+                       :slant :italic))
    (row1 capi:radio-button-panel
          :title "Writing:"
          :title-gap 30
          :title-position :left
-         :items (likert-button-list "Left" "" "Right")
+         :items (likert-button-list "Left" "no pref" "Right")
          :layout-args '(:gap 10))
    (row2 capi:radio-button-panel
           :title "Drawing:"
@@ -72,13 +103,48 @@
           :items (likert-button-list "Left" "" "Right")
           :layout-args '(:gap 10))
    (button-done capi:push-button
-                :text "Done"
+                :text "Evaluate"
                 :callback 'done-dialog
-                :default-p t))
+                :default-p t)
+   (np0 capi:display-pane
+         :text "                 "
+         :background :transparent)
+   (np1 capi:display-pane
+         :text ""
+         :background :transparent)
+   (np2 capi:display-pane
+         :text "no pref"
+         :background :transparent)
+   (oh1 capi:display-pane
+         :text "Do you ever use"
+         :background :gray75)
+   (oh2 capi:display-pane
+         :text "the other hand?"
+         :background :gray75)
+   (h1 capi:check-button :text "Yes    " :data "y1")
+   (h2 capi:check-button :text "Yes    " :data "y2")
+   (h3 capi:check-button :text "Yes    " :data "y3")
+   (h4 capi:check-button :text "Yes    " :data "y4")
+   (h5 capi:check-button :text "Yes    " :data "y5")
+   (h6 capi:check-button :text "Yes    " :data "y6")
+   (h7 capi:check-button :text "Yes    " :data "y7")
+   (h8 capi:check-button :text "Yes    " :data "y8")
+   (h9 capi:check-button :text "Yes    " :data "y9")
+   (h10 capi:check-button :text "Yes    " :data "y10"))
   (:layouts
-   (main capi:column-layout '(description inventory buttons) :adjust :left)
-   (inventory capi:column-layout '(row1 row2 row3 row4 row5 row6 row7 row8 row9 row10) :adjust :right :accessor inventory)
-   (buttons capi:row-layout '(nil button-done nil) :adjust :center)
+   (main capi:column-layout '(ta da ia ba) :adjust :left :gap 10)
+   (ta capi:row-layout '(nil title nil))
+   (da capi:column-layout '(instructions inst i0) :gap 15)
+   (i0 capi:column-layout '(i1 i2))
+   (i1 capi:row-layout '(nil inst1 nil) :adjust :center)
+   (i2 capi:row-layout '(nil inst2 nil) :adjust :center)
+   (ia capi:row-layout '(inventory otherhands) :title "Which hand do you prefer to use when:" :title-position :frame)
+   (inventory capi:column-layout '(row0 row1 row2 row3 row4 row5 row6 row7 row8 row9 row10) :adjust :right :accessor inventory)
+   (row0 capi:row-layout '(col0 np0))
+   (col0 capi:column-layout '(np1 np2))
+   (otherhands capi:column-layout '(h0 h1 h2 h3 h4 h5 h6 h7 h8 h9 h10) :adjust :right)
+   (h0 capi:column-layout '(oh1 oh2) :background :gray75)
+   (ba capi:row-layout '(nil button-done nil) :adjust :center)
    )
   (:default-initargs
    :title "The Edinburgh Handedness Inventory")
@@ -86,7 +152,12 @@
      
 (defmethod initialize-instance :after ((win edinburgh) &rest args)
   (let ((panels (capi:layout-description (inventory win))))
-    (dolist (panel panels)
-      (setf (capi:choice-selection (slot-value win panel)) nil))))
+    (dolist (panel (rest panels))
+      (let ((p (slot-value win panel)))
+        (setf (capi:choice-selection p) nil)
+        (setf (capi:simple-pane-background p) :gray85)))))
+;        (let ((buttons (capi:collection-items p)))
+ ;         (loop for button across buttons do
+  ;              (setf (capi:simple-pane-foreground button) :red)))))))
 
 (capi:display-dialog (make-instance 'edinburgh))
